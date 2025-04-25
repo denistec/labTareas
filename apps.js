67a2d4c5876fa8393b61fafdfcb1c9ccacea1a1e
denistec <yodygranadosrojas@hotmail.com>
2025-04-24 20:21:06 -0500

    subir funcionalidad que tacha la nueva tarea creada al hacer click

--- apps.js
+++ apps.js
@@ -1,4 +1,5 @@
 
+
 const input = document.getElementById("ingreso-texto");
 const lista = document.getElementById("lista-tareas");
 const crearBotonEliminar = document.createElement("button");
@@ -14,10 +15,13 @@ botonAgregar.addEventListener("click", function(){
     crearBotonEliminar.type = "button";
     crearBotonEliminar.textContent = "eliminar tarea";
     itemNuevo.appendChild(crearBotonEliminar) // agregar botón dentro del li
+        //agregar escuchador de evento al nuevo li (necesario para tachar la tarea más adelante)
+        itemNuevo.addEventListener("click", function() {
+            this.style.textDecoration="line-through";
+        })
 });
 //ELIMINAR TAREA
     crearBotonEliminar.addEventListener("click", function(){
         const li = this.parentElement; //Obtener el <li> que contiene el botón
         li.remove();
 });
-
